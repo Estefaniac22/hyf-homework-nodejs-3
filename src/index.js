6597103d19c1e89e680506e312140d0e9250b271
Estefania <estefaniac.25@hotmail.com>
2020-06-14 17:53:52 -0300

    add list of users

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ app.get('/', function(req, res){
 
 });
 
+
+app.get('/users', function(req,res){
+res.send([]);
+    console.log("running");
+
+});
+
 var server = app.listen(3000, function(){
     console.log("Server listening on port 3000");
 })
